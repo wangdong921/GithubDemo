4bcbb121adfb58e45b0eb6c0ae76e7a4f962f3ed
wangdong <873889098@qq.com>
2017-02-13 21:13:29 +0800

    修改： 添加toast 新增： 两个Activity

--- app/src/main/java/com/wangdong/githubdemo/ThirdActivity.java
+++ app/src/main/java/com/wangdong/githubdemo/ThirdActivity.java
@@ -2,15 +2,12 @@ package com.wangdong.githubdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toast.makeText(this,"Git test !!",Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_third);
     }
 }

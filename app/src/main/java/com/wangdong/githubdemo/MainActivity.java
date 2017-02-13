f1b9abe28bcbd4649e5ef20c009a131871a8c722
wangdong <873889098@qq.com>
2017-02-13 21:16:16 +0800

    Toast 显示时间改为long

--- app/src/main/java/com/wangdong/githubdemo/MainActivity.java
+++ app/src/main/java/com/wangdong/githubdemo/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this,"Git test !!",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Git test !!",Toast.LENGTH_LONG).show();
     }
 }
